d017f38ea8aec1fe04a6efd2238a8d1ebdac87f9
amirhossein <79211332+capamir@users.noreply.github.com>
2025-07-13 18:59:28 +0330

    telegram sdk added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import ResultsPage from './pages/ResultsPage';
+import WebApp from '@twa-dev/sdk';
 
 const App: React.FC = () => {
-  
+  useEffect(() => {
+    WebApp.ready();
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
